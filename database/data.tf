67a4babb883b4b3e8063c137b9cd564e4ce637d1
hugokishi <hugopacheco370@gmail.com>
2025-05-24 10:22:48 -0300

    feat: change to use terraform state

--- database/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "new-terraform-bucket"
-    key    = "vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- main.tf
+++ main.tf
@@ -1,28 +1,25 @@
 resource "aws_db_subnet_group" "main" {
   name       = "fiap-tc03-subnet"
-  subnet_ids = [
-    data.terraform_remote_state.vpc.outputs.public_subnet_a_id,
-    data.terraform_remote_state.vpc.outputs.public_subnet_b_id
-  ]
-
+  subnet_ids = terraform_remote_state.k8s.outputs.subnet_ids
+  
   tags = {
-    Name = "MainSubnetGroup"
+    Name = "RdsSubnetGroup"
   }
 }
 
 resource "aws_db_instance" "postgres" {
   allocated_storage       = 20
-  db_name                 = "mydatabase"
+  db_name                 = "fastfood_10soat_g22_tc3"
   engine                  = "postgres"
   engine_version          = "17.5"
   instance_class          = "db.t3.micro"
   username                = var.db_username
   password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.main.name
-  vpc_security_group_ids  = [data.terraform_remote_state.vpc.outputs.sg_id]
+  vpc_security_group_ids  = [data.terraform_remote_state.k8s.outputs.sg_id]
   publicly_accessible     = true
   skip_final_snapshot     = true
-  identifier              = "fiap-tc033"
+  identifier              = "fastfood_10soat_g22_tc3"
   backup_retention_period = 7  
   multi_az                = false
   storage_type            = "gp3"
