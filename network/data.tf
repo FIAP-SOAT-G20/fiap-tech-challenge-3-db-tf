0e3ab1341f4496084ac1123d8b1a2c1e2925b245
hugokishi <hugopacheco370@gmail.com>
2025-05-24 10:20:49 -0300

    feat: change database

--- network/data.tf
+++ network/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "new-terraform-bucket"
+    bucket = "fastfood-terraform-state-g22-tc3"
     key    = "vpc/terraform.tfstate"
     region = "us-east-1"
   }
